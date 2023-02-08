a03c8e89594c91590792b495dcb4d62020e3d099
Aditya Aggarwal <adityaaggarwal7861@gmail.com>
2023-02-09 01:39:42 +0530

    added ssl

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
-    ssl: process.env.SSL
+    ssl: process.env.DB_SSL
 });
 
 
